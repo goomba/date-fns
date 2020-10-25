67036857d10959bacd425916b1675da628cf9b04
Michael Yuen <iam@mikey.baby>
2020-10-25 18:15:13 -0400

    Functional formatDuration!

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,99 @@
 import { intervalToDuration } from "date-fns";
+// import { es } from "date-fns/locale";
 
-const MILLISECOND = 1;
-const SECOND = MILLISECOND * 1000;
-const MINUTE = SECOND * 60;
-const HOUR = MINUTE * 60;
-const DAY = HOUR * 24;
-const WEEK = DAY * 7;
-const MONTH = WEEK * 4;
-const YEAR = MONTH * 12;
+type SupportedTokens = "y" | "M" | "w" | "d" | "h" | "m" | "s";
+
+enum MS_IN {
+	MILLISECOND = 1,
+	SECOND = MS_IN.MILLISECOND * 1000,
+	MINUTE = MS_IN.SECOND * 60,
+	HOUR = MS_IN.MINUTE * 60,
+	DAY = MS_IN.HOUR * 24,
+	WEEK = MS_IN.DAY * 7,
+	MONTH = MS_IN.WEEK * 4,
+	YEAR = MONTH * 12,
+}
+
+export function format(): string {
+	return "hi";
+}
+
+type GetTimestampProps = {
+	date: string;
+	baseDate: string;
+	format: string;
+};
+
+export function getTimestamp({
+	date,
+	baseDate,
+	format,
+}: GetTimestampProps): null | string {
+	const start = new Date(date);
+	const end = new Date(baseDate);
+	const duration = intervalToDuration({ start, end });
+	return formatDuration(duration as DurationType, format);
+}
+
+type DurationConfig = {
+	[index: string]: {
+		converter: Converter;
+		key: string;
+		ms: number;
+		order: number;
+	};
+};
+
+const durationConfig: DurationConfig = {
+	y: {
+		converter: makeConverter("y"),
+		key: "years",
+		ms: MS_IN.YEAR,
+		order: 0,
+	},
+	M: {
+		converter: makeConverter("M"),
+		key: "months",
+		ms: MS_IN.MONTH,
+		order: 1,
+	},
+	d: {
+		converter: makeConverter("d"),
+		key: "days",
+		ms: MS_IN.DAY,
+		order: 2,
+	},
+	h: {
+		converter: makeConverter("h"),
+		key: "hours",
+		ms: MS_IN.HOUR,
+		order: 3,
+	},
+	m: {
+		converter: makeConverter("m"),
+		key: "minutes",
+		ms: MS_IN.MINUTE,
+		order: 4,
+	},
+	s: {
+		converter: makeConverter("s"),
+		key: "seconds",
+		ms: MS_IN.SECOND,
+		order: 5,
+	},
+};
+
+type DurationQuantityMap = {
+	[key: string]: number | undefined;
+	y?: number;
+	M?: number;
+	w?: number;
+	d?: number;
+	h?: number;
+	m?: number;
+	s?: number;
+	milliseconds: number;
+};
 
 /**
  * This RegExp consists of three parts separated by `|`:
@@ -27,96 +113,70 @@ const escapedStringRegExp = /^'([^]*?)'?$/;
 const doubleQuoteRegExp = /''/g;
 const unescapedLatinCharacterRegExp = /[a-zA-Z]/;
 
-export function format(): string {
-	return "hi";
-}
-
-type GetTimestampProps = {
-	date: string;
-	baseDate: string;
-	format: string;
-};
-
-export function getTimestamp({
-	date,
-	baseDate,
-	format,
-}: GetTimestampProps): string {
-	const start = new Date(date);
-	const end = new Date(baseDate);
-	const duration = intervalToDuration({ start, end });
-	return formatDuration(duration as DurationType, format);
-}
-
-type DurationConfig = {
-	[index: string]: {
-		key: string;
-		ms: number;
-		order: number;
-	};
-};
-
-const durationConfig: DurationConfig = {
-	y: {
-		// converter: makeConverter('y');
-		key: "years",
-		ms: YEAR,
-		order: 0,
-	},
-	M: {
-		key: "months",
-		ms: MONTH,
-		order: 1,
-	},
-	d: {
-		key: "days",
-		ms: DAY,
-		order: 2,
-	},
-	h: {
-		key: "hours",
-		ms: HOUR,
-		order: 3,
-	},
-	m: {
-		key: "minutes",
-		ms: MINUTE,
-		order: 4,
-	},
-	s: {
-		key: "seconds",
-		ms: SECOND,
-		order: 5,
-	},
-};
-
-export function formatDuration(duration: DurationType, format: string): any {
-	// eslint-disable-line
-	console.log(duration);
+export function formatDuration(
+	duration: DurationType,
+	format: string
+): null | string {
 	const milliseconds = durationToMilliseconds(duration);
-
 	const tokenMatches = format.match(durationTokensRegExp);
-	console.log(tokenMatches);
 
 	if (!tokenMatches) {
 		console.error("NO TOKEN MATCHES");
 		return null;
 	}
 
+	/**
+	 * 1. Filter down to _just_ the duration tokens
+	 * 2. Order them from largest to smallest (e.g., hours > minutes > seconds)
+	 * 3. Reduce to durationQuantityMap
+	 *   - For the ordered tokens, use the total ms from durationToMilliseconds
+	 *     and dynamically calculate duration of each part "present" in the format
+	 */
 	const durationQuantityMap = tokenMatches
 		.filter((substring) => durationConfig[substring[0]])
-		.sort((a, b) => durationConfig[a[0]].order - durationConfig[b[0]].order);
-	// .reduce(
-	// 	(quantityMap, token) => {
-	// 		return {
-	// 			...quantityMap,
-	// 			...durationConversionMap[token](quantityMap.milliseconds),
-	// 		};
-	// 	},
-	// 	{ milliseconds }
-	// );
+		.sort((a, b) => durationConfig[a[0]].order - durationConfig[b[0]].order)
+		.reduce(
+			(quantityMap: DurationQuantityMap, substring: string) => ({
+				...quantityMap,
+				...durationConfig[substring[0] as SupportedTokens].converter(
+					quantityMap.milliseconds
+				),
+			}),
+			{ milliseconds }
+		);
 
-	return durationQuantityMap;
+	/**
+	 * This block is copies most of the `format` logic: https://github.com/date-fns/date-fns/blob/master/src/format/index.js#L412
+	 * But replaces tokens with their appropriate duration.
+	 */
+	const result = tokenMatches
+		.map((substring) => {
+			// Replace two single quote characters with one single quote character
+			if (substring === "''") {
+				return "'";
+			}
+			const firstCharacter = substring[0];
+			if (firstCharacter === "'") {
+				return cleanEscapedString(substring);
+			}
+
+			const duration = durationQuantityMap[firstCharacter];
+			if (typeof duration !== "undefined") {
+				return addLeadingZeros(duration, substring.length);
+			}
+
+			if (firstCharacter.match(unescapedLatinCharacterRegExp)) {
+				throw new RangeError(
+					"Format string contains an unescaped latin alphabet character `" +
+						firstCharacter +
+						"`"
+				);
+			}
+
+			return substring;
+		})
+		.join("");
+	return result;
 }
 
 type DurationType = {
@@ -130,6 +190,14 @@ type DurationType = {
 	seconds: number;
 };
 
+const cleanEscapedString = (input: string) => {
+	const escapedStringMatch = input.match(escapedStringRegExp);
+	if (escapedStringMatch) {
+		return escapedStringMatch[1].replace(doubleQuoteRegExp, "'");
+	}
+	return input;
+};
+
 export function durationToMilliseconds(duration: DurationType): number {
 	return Object.keys(durationConfig).reduce(
 		(milliseconds, durationConfigProperty) => {
@@ -138,4 +206,27 @@ export function durationToMilliseconds(duration: DurationType): number {
 		},
 		0
 	);
+}
+
+type Converter = (ms: number) => DurationQuantityMap;
+
+function makeConverter(token: SupportedTokens): Converter {
+	return function (ms: number) {
+		const { ms: msPerToken } = durationConfig[token];
+		return {
+			[token]: Math.floor(ms / msPerToken),
+			milliseconds: ms % msPerToken,
+		};
+	};
+}
+
+function addLeadingZeros(number: number, targetLength: number): string {
+	const sign = number < 0 ? "-" : "";
+	let output = Math.abs(number).toString();
+
+	while (output.length < targetLength) {
+		output = `0${output}`;
+	}
+
+	return sign + output;
 }
--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { getTimestamp, formatDuration } from "./src";
+import { getTimestamp } from "./src";
 
 const date = "2020-10-20T04:10:00Z";
 const baseDate = "2020-10-19T00:08:29Z";
@@ -6,7 +6,7 @@ const baseDate = "2020-10-19T00:08:29Z";
 const result = getTimestamp({
 	date,
 	baseDate,
-	format: "s 'seconds,' h 'hours'",
+	format: "dd 'days', s 'seconds,' h 'hours'",
 });
 
-console.log("📅 \u001b[" + 32 + "m" + `${result}` + "\u001b[0m");
+console.log("📅 \u001b[" + 32 + "m" + `${result}` + "\u001b[0m"); // eslint-disable-line
