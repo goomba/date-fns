afff31be124725f5b863ef0622492f13d4d0975f
Michael Yuen <iam@mikey.baby>
2020-12-21 20:06:45 -0500

    Fix durationToMilliseconds to support partial duration objects (#3)

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
-import { format } from "./src";
+import { format, formatDuration } from "./src";
 
 const date = "2020-10-20T04:10:00Z";
 const baseDate = "2020-10-19T00:08:29Z";
 
-const result = format({
-	date,
-	baseDate,
-	format: "d 'days', s 'seconds,' h 'hours'",
-});
+// const result = format({
+// 	date,
+// 	baseDate,
+// 	format: "d 'days', s 'seconds,' h 'hours'",
+// });
+
+const result = formatDuration({ days: 3 }, "d 'days ago'");
 
 console.log("📅 \u001b[" + 32 + "m" + `${result}` + "\u001b[0m"); // eslint-disable-line
