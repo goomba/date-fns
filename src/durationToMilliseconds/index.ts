a15ce4cc0228aa541b4f8e97473258c3bbcaa661
Michael Yuen <iam@mikey.baby>
2020-12-21 20:03:22 -0500

    Fix durationToMilliseconds to support partial duration objects

--- src/durationToMilliseconds/index.ts
+++ src/durationToMilliseconds/index.ts
@@ -5,7 +5,7 @@ export function durationToMilliseconds(duration: DurationWithIndex): number {
 	return Object.keys(durationConfig).reduce(
 		(milliseconds, durationConfigProperty) => {
 			const { key, ms } = durationConfig[durationConfigProperty];
-			return milliseconds + duration[key] * ms;
+			return duration[key] ? milliseconds + duration[key] * ms : milliseconds;
 		},
 		0
 	);
